cf73a2eb181f0912a3b96f0faab579aa974380ba
Marisa Refe <102203048+M-Refe@users.noreply.github.com>
2022-07-08 17:58:37 -0700

    day 17 notes

--- whileloopscript.py
+++ whileloopscript.py
@@ -1,1 +1,24 @@
 #while loop is similar to an if statement
+#while loop will continue to execute as long as the condition is true
+
+#it executes whil that thing is true
+
+count = 0
+
+if count < 5:
+  print ("Hello, I am an if statement and count is"), count
+
+while count <= 9:
+  print ("Hello, I am a while and count is"), count
+  count += 1
+
+#the while loop above will print out the statement until the count goes to number 10, and then it will completely stop looping
+
+#Loop condition
+# condition is the expression that decides whether the loop is going to continue being exececuted or not
+# 5 steps
+    #loop_condition variable is set to True
+    #the while loop checks to see if loop_condition is True.  It is, so the loop is entered
+    #the print statement is executed
+    #the variable loop_condition is set to False
+    #The while loop again checks to see if loop_condition is True. 

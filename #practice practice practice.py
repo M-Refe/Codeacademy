f28d32a14c30863a4f491fe6c22d19fb264aa7aa
Marisa Refe <102203048+M-Refe@users.noreply.github.com>
2022-07-19 16:43:02 -0700

    day 21 practice

--- #practice practice practice.py
+++ #practice practice practice.py
@@ -70,3 +70,109 @@ def factorial(x):
     return total
   
 print (factorial(5))
+
+#is_prime function
+#a prime number is a positive integer greater than 1 that has no positive divisors other than 1 and itself.
+#my code that semi works
+def is_prime(x):
+  for n in range(2, x-1):
+    if x % n:
+      return False
+    else: 
+      return True
+print (is_prime(5))  #it returned False - so it works, but it's resulting error because 0 is coming out as none
+
+def is_prime(x):
+  if x < 2:
+    return False
+  else:
+    for n in range(2, x-1):
+      if x % n == 0:
+        return False
+    return True
+print (is_prime(9))
+
+# reverse
+#i honestly don't get this part without using Pro
+def reverse(text):
+    word = ""
+    l = len(text) - 1
+    while l >= 0:
+        word = word + text[l]
+        l -= 1
+    return word
+  
+print (reverse("Hello World"))
+
+#anti_vowel
+def anti_vowel(text):
+    result = ""
+    vowels = "ieaouIEAOU"
+    for char in text:
+          if char not in vowels:
+            result += char
+    return result
+print (anti_vowel("hello book"))
+
+#scrabble_score
+#my code
+#define scrabble_score that takes the a string "word" as input and returns the equivalent scrabble score for that word
+score = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2, 
+         "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3, 
+         "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1, 
+         "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4, 
+         "x": 8, "z": 10}
+
+def scrabble_score(word):
+  word = ""
+  word = word.lower()
+  total = 0
+  for word in score:
+    print (score[word])
+    total = total + score[word]
+    return total
+print (scrabble_score("pie"))
+
+#solution
+def scrabble_score(word):
+  word = word.lower()
+  total = 0
+  for letter in word:
+    for leter in score:
+      if letter == leter:
+        total = total + score[leter]
+  return total
+
+print (scrabble_score("pizza"))
+
+#censor
+#write function called "censor", that takes two strings, 'text' and 'word' as input. It should return the 'text' with the 'word' you chose replaced with asterisks.
+#i've literally don't understand this part of the course. I've not learned any of this. and none is in my notes
+def censor(text, word):
+    words = text.split()
+    result = ''
+    stars = '*' * len(word)
+    count = 0
+    for i in words:
+        if i == word:
+            words[count] = stars
+        count += 1
+    result =' '.join(words)
+
+    return result
+  
+print (censor("this hack is wack hack", "hack"))
+#it took the word "hack" and replaced it with ***
+
+
+#define a function called count that has 2 arguments "sequence" and "item"
+#return the number of times the item occurs in the list
+#for example: count([1, 2, 1, 1], 1) should return 3 (because 1 appears 3 times in the list)
+def count(sequence, item):
+    count = 0
+    for i in sequence:
+        if i == item:
+            count += 1
+    return count
+  
+print (count([1, 2, 1, 1], 1))

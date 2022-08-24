17bf623ef22c28fca428d13edad38ecd5932bae8
Marisa Refe <102203048+M-Refe@users.noreply.github.com>
2022-08-23 23:19:58 -0700

    end of day notes

--- Python 3.py
+++ Python 3.py
@@ -2613,3 +2613,46 @@ def remove_middle(lst, start, end):
 """
 The function should return a list where all elements in lst with an index between start and end (inclusive) have been removed.
 """
+
+#counting items in list
+def more_frequent_item(lst, item1, item2):
+  if lst.count(item1) >= lst.count(item2):
+    return item1
+  else:
+    return item2
+
+"""
+Create a function named more_frequent_item that has three parameters named lst, item1, and item2.
+Return either item1 or item2 depending on which item appears more often in lst.
+"""
+
+#double index
+
+def double_index(lst, index):
+  if index >= len(lst):
+    return lst
+  else:
+    new_lst = lst[0:index]
+    new_lst.append(lst[index]*2)
+    new_lst = new_lst + lst[index+1:]
+    return new_lst
+
+"""
+did not get this one whatsoever
+
+Our next function will double a value at a given position. We will provide a list and an index to double. 
+This will create a new list by replacing the value at the index provided with double the original value. 
+If the index is invalid then we should return the original list. Here is what we need to do:
+
+  Define the function to accept two parameters, one for the list and another for the index of the value we are going to double
+  Test if the index is invalid. If its invalid then return the original list
+  If the list is valid then get all values up to the index and store it as a new list
+  Append the value at the index times 2 to the new list
+  Add the rest of the list from the index onto the new list
+  Return the new list
+
+Create a function named double_index that has two parameters: a list named lst and a single number named index.
+The function should return a new list where all elements are the same as in lst except for the element at index. 
+The element at index should be double the value of the element at index of the original lst.
+If index is not a valid index, the function should return the original list.
+"""
